b290c3542c5dc2d6c9acfc602e5e23716334ef66
Rockson Zeta <rocksonzeta@gmail.com>
2019-04-15 11:25:40 +0800

    Update servicePool.go

--- kv/servicePool.go
+++ kv/servicePool.go
@@ -13,14 +13,7 @@ import (
 // ErrKeyNotFound = errors.New("Key '%s' doesn't found")
 // )
 type RedisConfig struct {
-	// Network "tcp"
-	Network string
-	// Addr "127.0.0.1:6379"
-	Addr string
-	// Password string .If no password then no 'AUTH'. Default ""
-	Password string
-	// If Database is empty "" then no 'SELECT'. Default ""
-	Database string
+	Url string
 	// MaxIdle 0 no limit
 	MaxIdle int
 	// MaxActive 0 no limit
@@ -54,37 +47,14 @@ func (s *ServiceFactory) initPool() {
 		c.IdleTimeout = time.Duration(5) * time.Minute
 	}
 
-	if c.Network == "" {
-		c.Network = "tcp"
-	}
-
-	if c.Addr == "" {
-		c.Addr = "127.0.0.1:6379"
-	}
-
 	Pool := &redis.Pool{IdleTimeout: time.Duration(5) * time.Minute, MaxIdle: c.MaxIdle, MaxActive: c.MaxActive}
 	Pool.TestOnBorrow = func(c redis.Conn, t time.Time) error {
 		_, err := c.Do("PING")
 		return err
 	}
 
-	if c.Database != "" {
-		Pool.Dial = func() (redis.Conn, error) {
-			red, err := dial(c.Network, c.Addr, c.Password)
-			if err != nil {
-				return nil, err
-			}
-			if _, err = red.Do("SELECT", c.Database); err != nil {
-				red.Close()
-				return nil, err
-			}
-			return red, err
-		}
-	} else {
-		Pool.Dial = func() (redis.Conn, error) {
-			return dial(c.Network, c.Addr, c.Password)
-		}
+	Pool.Dial = func() (redis.Conn, error) {
+		return redis.DialURL(c.Url)
 	}
-	// r.Connected = true
 	s.Pool = Pool
 }
